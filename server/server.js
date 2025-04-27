fde1713e6ab28eea31dd15247bf4a912c0a5f95f
sushant Aryal <aryasushant7@gmail.com>
2025-04-27 07:19:27 +0530

    done..

--- server/server.js
+++ server/server.js
@@ -10,7 +10,6 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
@@ -22,11 +21,8 @@ app.use("/api/tasks", taskRoutes);
 // MongoDB Atlas connection
 mongoose
   .connect(process.env.MONGO_URI)
-
   .then(() => {
     console.log("MongoDB Connected");
-    app.listen(PORT, () =>
-      console.log("Server running on port 5000 jjjjjjjjjj")
-    );
+    app.listen(PORT, () => console.log(`Server kkkkk  kkkkkkkkk  running on port ${PORT}`));
   })
   .catch((err) => console.log(err));
